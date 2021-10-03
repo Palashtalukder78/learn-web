c16f1f4d260a45d3a7ab93a6a19209c77de3cc2e
Palashtalukder78 <palashtalukder78@gmail.com>
2021-10-04 03:14:23 +0600

    Adding rating and delete unnecessary photo

--- src/components/SingleService/SingleService.js
+++ src/components/SingleService/SingleService.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Rating from 'react-rating';
+import './SingleService.css';
 const SingleService = (props) => {
-    const { title, photo, price } = props.service;
+    const { title, photo, price, rating, availableSit } = props.service;
     return (
         <div className="col-md-6">
-            <div class="card mb-3">
+            <div class="card mb-3 item shadow">
                 <div class="row g-0">
                     <div class="col-md-5">
                         <img src={photo} class="img-fluid rounded-start service-photo" style={{ height: "150px" }} alt="..." />
@@ -12,8 +13,14 @@ const SingleService = (props) => {
                     <div class="col-md-7">
                         <div class="card-body">
                             <h5 class="card-title">{title}</h5>
-                            <p class="card-text">Price: $ {price}</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <p class="card-text mb-0">Price: <b>$ {price}</b></p>
+                            <p class="card-text  m-0"><small class="text-muted">Only {availableSit} sit available. Enroll Fast</small></p>
+                            <Rating
+                                initialRating={rating}
+                                emptySymbol="far fa-star empty-star"
+                                fullSymbol="fas fa-star full-star"
+                                readonly
+                            />
                         </div>
                     </div>
                 </div>
