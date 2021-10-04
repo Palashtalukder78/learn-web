180daa03adaaaf5e486f6911401ad01d535de53d
Palashtalukder78 <palashtalukder78@gmail.com>
2021-10-04 17:43:41 +0600

    Delete unnecessary Import

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
-import Instructors from '../Instructors/Instructors';
 import Service from '../Service/Service';
 
 const Home = () => {
