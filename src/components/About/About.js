9bad2e12881a377ab987137c62a1cca12561614c
Palashtalukder78 <palashtalukder78@gmail.com>
2021-10-04 11:51:11 +0600

    Add spinner for about-instruction

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Spinner } from 'react-bootstrap';
 import Banner from '../Banner/Banner';
 import Instructors from '../Instructors/Instructors';
 
@@ -22,7 +22,11 @@ const About = () => {
                 </div>
                 <h3 className="text-center">Our Isctructors</h3>
                 <div className="row my-4">
-                    {
+                    {instructors.length === 0 ?
+                        <div className="text-center">
+                            <Spinner animation="grow" variant="dark" />
+                        </div>
+                        :
                         instructors.map(instructor => <Instructors
                             key={instructor.id}
                             instructor={instructor}
